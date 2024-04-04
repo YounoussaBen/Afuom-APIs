95e1414e3f8c7e039bbd4d8605b97ae5404341b2
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-04-04 23:13:53 +0000

    authentications

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,6 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import CreateUserView
+from .views import CreateUserView, GoogleLogin
+from allauth.socialaccount.views import signup
 from django.urls import path
 
 
@@ -7,4 +8,6 @@ urlpatterns = [
     path("user/register/", CreateUserView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="get_token"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("signup/", signup, name="socialaccount_signup"),
+    path("google/", GoogleLogin.as_view(), name="google_login"),
 ]
