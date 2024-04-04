2b155965ff2c7a1fc909a25eba0085bd5d105f85
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-04-04 23:13:19 +0000

    setting up urls

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path("auth/", include("authentication.urls")),
     path("shop/", include("shop.urls")),
     path("wishlist/", include("wishlist.urls")),
+    path("cart/", include("cart.urls")),
+    path("order/", include("order.urls")),
 ]
