a642892b4e64b9bc256a8daf550a4cfd3edbd7d5
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-04-13 12:39:17 +0000

    UPdate Order Serializer

--- order/serializers.py
+++ order/serializers.py
@@ -25,4 +25,12 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         exclude = ("is_deleted",)
         read_only_fields = ("created", "updated", "created_by")
+    
+    def create(self, validated_data):
+        # Get the user who is creating the product
+        user = self.context['request'].user if 'request' in self.context else None
 
+        # Set the created_by field with the user
+        validated_data['created_by'] = user
+
+        return super().create(validated_data)
