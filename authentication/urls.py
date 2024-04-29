e97d753c35d15793a110349382c5c88b2e9065b8
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-04-29 14:43:24 +0000

    Users endpoints

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import CreateUserView
+from .views import CreateUserView, ListUserView, RetrieveUpdateDeleteUserView
 from django.urls import path
 
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path("user/register/", CreateUserView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="get_token"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("user/list/", ListUserView.as_view(), name="list"),
+    path("user/<int:pk>/", RetrieveUpdateDeleteUserView.as_view(), name="retrieve_update_delete"),
 ]
